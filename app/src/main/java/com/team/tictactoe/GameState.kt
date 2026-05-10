85dc0fa70f412d5556d489dbaa36747162f413e2
Stas <finskij18ass@gmail.com>
2026-05-10 16:52:56 +0300

    Added GameDebug, GameRules docs and edge-case checks

--- app/src/main/java/com/team/tictactoe/GameState.kt
+++ app/src/main/java/com/team/tictactoe/GameState.kt
@@ -1,5 +1,13 @@
 package com.team.tictactoe
-
 data class GameState(
     val board: List<Player?> = List(9) { null },
+    val currentTurn: Player = Player.HUMAN,
+    val status: GameStatus = GameStatus.PLAYING
 )
+
+enum class GameStatus {
+    PLAYING,
+    HUMAN_WIN,
+    AI_WIN,
+    DRAW
+}
